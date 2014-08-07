e2e0faa0a12cb291b57545f3e06f5f0427fddcd9
Martin Wang <wangwei917@gmail.com>
2014-08-07 10:53:33 +0800

    Update tmux-dev.sh

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -19,14 +19,15 @@ tmux new-window -t $SESSION:1 -n portal
 tmux new-window -t $SESSION:2 -n cloudengine 
 tmux new-window -t $SESSION:3 -n scheduler 
 tmux new-window -t $SESSION:4 -n worker 
-tmux new-window -t $SESSION:5 -n martin 
+tmux new-window -t $SESSION:5 -n faye 
 tmux new-window -t $SESSION:6 -n root 
 
-tmux send-keys -t $SESSION:0 'cd ~/projects; gvim' C-m
+tmux send-keys -t $SESSION:0 'cd ~/projects; vim' C-m
 tmux send-keys -t $SESSION:1 'cdportal; rails s' C-m
 tmux send-keys -t $SESSION:2 'cdce; rackup' C-m
 tmux send-keys -t $SESSION:3 'cdce; rake resque:scheduler' C-m
 tmux send-keys -t $SESSION:4 'cdce; rake resque:work' C-m
+tmux send-keys -t $SESSION:5 'cdportal; foreman s' C-m
 tmux send-keys -t $SESSION:6 'sudo su -' C-m
 
 tmux select-window -t $SESSION:0
